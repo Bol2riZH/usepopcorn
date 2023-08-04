da49a0313a0264ca0624a7fe2ae70605d0c67c69
Matthieu REUNGOAT <reungoat.matthieu@gmail.com>
2023-08-04 14:02:28 +0200

    100%

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const containerStyle = {
   display: 'flex',
@@ -8,6 +9,16 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: 'flex',
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
 
 const Star = ({
@@ -65,12 +76,21 @@ const Star = ({
   );
 };
 
-const StarRating = ({ maxRating = 5, color = '#fcc419', size = 48 }) => {
-  const [rating, setRating] = useState(0);
+const StarRating = ({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) => {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   const handleRating = rating => {
     setRating(rating);
+    onSetRating(rating);
   };
 
   const textStyle = {
@@ -81,7 +101,10 @@ const StarRating = ({ maxRating = 5, color = '#fcc419', size = 48 }) => {
   };
 
   return (
-    <div style={containerStyle}>
+    <div
+      style={containerStyle}
+      className='test'
+    >
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -90,10 +113,16 @@ const StarRating = ({ maxRating = 5, color = '#fcc419', size = 48 }) => {
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ''}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ''}
+      </p>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import ReactDOM from 'react-dom/client';
 import StarRating from './components/StarRating';
 // import './index.css';
 // import App from './App';
 
+function Test() {
+  const [rating, setRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        color='blue'
+        size={24}
+        defaultRating={3}
+        onSetRating={setRating}
+      />
+      <p>This movie is {rating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={['terrible', 'bad', 'okay', 'good', 'great']}
+    />
+    <StarRating
+      size={24}
+      defaultRating={3}
+      color='red'
+      className='test'
+    />
+    <Test />
   </React.StrictMode>
 );
